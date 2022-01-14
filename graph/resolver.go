710ff78a8ab848ed9c50717c97c200e3a3fa7835
Ali Almohammad <ali@iotechsys.com>
2022-01-14 13:49:52 +0000

    Added files

---
+++ graph/resolver.go
@@ -0,0 +1,14 @@
+//go:generate go get github.com/99designs/gqlgen/cmd@v0.14.0
+//go:generate go run github.com/99designs/gqlgen
+
+package graph
+
+import "github.com/Ali-iotechsys/gqlgen-example/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	todos []*model.Todo
+}
